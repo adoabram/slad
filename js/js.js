866a55f3d79f463e5d1e25d0af7e5346391dcd61
adoabram <59774786+adoabram@users.noreply.github.com>
2021-04-20 21:37:52 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,14 @@
+jQuery(function($){
+	"use strict";
+	 $('.header_slider').slick({
+        arrows:false,
+		autoplay:true,
+		autoplaySpeed:2000,
+		infinite:true,
+		speed:2000,
+		fade: true,
+		cssEase:'linear',
+		dots:true
+	});	
+});
+	
